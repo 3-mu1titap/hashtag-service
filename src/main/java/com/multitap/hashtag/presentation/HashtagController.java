841118f86f6172e77bbfab5bdfcd74691ac93fe0
kang-subin <tnbin98@gmail.com>
2024-11-26 10:32:19 +0900

    Feat: todo 생성

--- src/main/java/com/multitap/hashtag/presentation/HashtagController.java
+++ src/main/java/com/multitap/hashtag/presentation/HashtagController.java
@@ -23,6 +23,7 @@ public class HashtagController {
 
     private final HashtagService hashtagService;
 
+    //todo: 해시태그, 피드백, 리뷰 키워드 등록 할 지 고민중
     @Operation(summary = "해시태그 등록", description = "해시태그를 등록합니다")
     @PostMapping("/hashtag")
     public BaseResponse<Void> addHashtag(@RequestBody HashtagRequestVo hashtagRequestVo) {
